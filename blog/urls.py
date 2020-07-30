a3f017404abc39914363b4c78779d9f356471389
Ujah Emmanuel <ujahemmanuel72@gmail.com>
2020-07-30 01:54:19 +0100

    Initial commit add blog and portfolio apps

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.all_blog_posts, name='all_blog_posts'),
+    path('<int:blog_id>/', views.detail, name='detail')
+
+]
---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+# Create your views here.
+
+
+def all_blog_posts(request):
+    blog = Blog.objects.order_by('-date')
+    return render(request, 'blog/all_blog_posts.html', {'blogs': blog})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
